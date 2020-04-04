5f9abccee03593f13c75711756a26e0df0d6cc37
Derek Pecka <dpecka@apple.com>
2020-04-03 22:26:55 -0500

    generalize callback

--- sendgrid-router.py
+++ sendgrid-router.py
@@ -1,25 +1,30 @@
 import json
 import subprocess
-
-from flask import Flask, request
+from flask import Flask, request, Response
 
 
 app = Flask(__name__)
 
+# proxy = 'http://localhost:80'
+proxy = None
+
 
 @app.route('/_receive', methods=['POST'])
-def hello_world():
+def event_receive():
     events = json.loads(request.data.decode('utf-8'))
     for event in events:
-        print(json.dumps(event))
-
         if 'source-callback' in event:
             callback_url = event['source-callback']
-            subprocess.check_output([
-                'curl', '-X', 'POST', '--proxy', 'http://localhost:80', '-d', json.dumps(event), callback_url
-            ])
+            if proxy:
+                subprocess.check_output([
+                    'curl', '-X', 'POST', '--proxy', proxy, '-d', json.dumps(event), callback_url
+                ])
+            else:
+                subprocess.check_output([
+                    'curl', '-X', 'POST', '-d', json.dumps(event), callback_url
+                ])
 
-    return 'Ack'
+    return Response(status_code=200)
 
 
 if __name__ == '__main__':
